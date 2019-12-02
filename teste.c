6e76c27f33fe73d3737cc3a531a4c6c9d1131063
gaeta <giovanna.gaeta@alunos.ifsuldeminas.edu.br>
2019-12-02 15:30:19 -0300

    first commit

---
+++ teste.c
@@ -0,0 +1,78 @@
+int a( int x ,int y ){
+   return x+y;
+}
+
+float by( ){
+   int b;
+   b=b*2;
+   return b;
+}
+
+void c( ,float z ){
+   int m;
+   int n;
+while((z>m&&z<n)||(m*n)>z){
+   z=z-1;
+}
+   
+}
+
+void d( ){
+   int o;
+for(o=1;o<10;o+=1){
+      printf("%d\n",o);
+}
+   
+}
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+   int p[10];
+   int q=6;
+   float r;
+   int t[10];
+   int g;
+   int h;
+   int ay;
+   int cy;
+   int byy;
+   int i;
+   p[1]=5;
+   float s;
+   p[1]=q/(5+1);
+   r=2.6*(r-2);
+   printf("%d\n",(p[1]+q)*p[1]);
+   printf("%f\n",r+2+cy);
+   t[1]=83+2+5+6+8+0;
+   t[1]=t+3;
+   scanf("%d",&g);
+   scanf("%d",&h);
+   ay=ay+byy;
+   if(p[1]>q){
+      printf("%f\n",r);
+      if(r==5.8){
+         printf("%d\n",p[1]);
+      }
+      else{
+         printf("%d\n",q);
+      }
+   }
+   else{
+      printf("%f\n",r/2);
+   }
+   a(p);
+   s=a();
+   s=by();
+   c(r);
+   d();
+for(i=1;i<10;i+=1){
+      printf("%d\n",i);
+}
+while((ay>cy&&byy>cy)||cy>ay){
+   printf("%d\n",ay*10);
+   ay=ay-1;
+}
+return 0;
+}
